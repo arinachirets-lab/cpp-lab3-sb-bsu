d6e9613e05938a1615698337a315ae2c808cb241
arinachirets-lab <arinachirets@gmail.com>
2025-10-08 19:24:44 +0300

    Update 3a-t4.cpp
    Add integer input validation

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,22 +1,32 @@
 #include<iostream>
+#include<limits>
 using namespace std;
+
 int main()
 {
     int k, i = 1, sum_k = 0, number;
     cout << "Enter the amount of integers to sum = ";
     cin >> k;
-    
+
     if (k <= 0) {
         cout << "No numbers to sum. Total sum is 0." << endl;
+        return 0;
     }
-    else {
-        do {
-            cout << "Enter integer no. " << i << ": ";
-            cin >> number;
-            sum_k += number;  
-            i++;
-        } while (i <= k);
-        cout << "The total sum of " << k << " integers is: " << sum_k;  
-    }
+
+    do {
+        cout << "Enter integer no. " << i << ": ";
+        
+        // Проверка корректного ввода
+        while (!(cin >> number)) {
+            cout << "Warning: Invalid input. Please enter an integer: ";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        
+        sum_k += number;
+        i++;
+    } while (i <= k);
+
+    cout << "The total sum of " << k << " integers is: " << sum_k;
     return 0;
 }
